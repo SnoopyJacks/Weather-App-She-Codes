edfb9931192acb36dca481b278df97b1ef0bd4d3
Justine <t.nichole.2807@gmail.com>
2026-04-15 07:18:52 -0600

    trying to debug js.  numbers arent showing on screen

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind-speed");
+  let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
